a9891203a8cba016ada197a6163c97f260b5cb65
Reggie899 <regina.schmidt1212@gmail.com>
2022-05-29 15:55:25 +0200

    update

--- src/Dictionary1.js
+++ src/Dictionary1.js
@@ -13,10 +13,11 @@ function handleResponse(response) {
 function search(event) {
     event.preventDefault();
 // alert(`Searching for ${keyword} defnition`)
-}
-
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
+}
+
+
 
 
     return (
