82c0453f00edb573bb0fedf1f5c8bd7aa1f2055a
Reggie899 <regina.schmidt1212@gmail.com>
2022-06-05 19:29:03 +0200

    submit meaning component

--- src/Result.js
+++ src/Result.js
@@ -1,3 +1,8 @@
+import React from "react";
+
+import Meaning from "./Meaning";
+
+
 export default function Result(props) {
   console.log("Result", props.result);
 
@@ -5,7 +10,11 @@ export default function Result(props) {
     return <div className="Results">
 
 <h2>{props.result.word}</h2>
-
+{props.result.meanings.map(function(meaning, index){
+  return <div key={index}>
+    <Meaning meaning={meaning} />
+    </div>  // meaning.definitions[0].definition;
+})}
     </div>;
   } else {
       return null;
