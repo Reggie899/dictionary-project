8b420c6e68c9716a1acd8cf14f52106fa17b8385
Reggie899 <regina.schmidt1212@gmail.com>
2022-06-06 23:50:06 +0200

    update

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,18 +1,27 @@
 import React from "react";
 
 export default function Phonetic(props) {
-    console.log("PHONNNN" , props.phonetic)
-    console.log("AUDIO" , props.phonetic.audio)
+  console.log("PHONNNN", props.phonetic);
+  console.log("AUDIO", props.phonetic.audio.length);
 
-  return (
-      <div style={{display: "flex", justifyContent: "center", margin: "6%"}}>
-<br/>
-<div style={{margin: "2%"}}>
-{props.phonetic.text}
-</div>
-<br/>
-<audio src={props.phonetic.audio} type="audio/mpeg" controls>
-</audio>
+  if (props.phonetic.text) {
+    return (
+      <div style={{ display: "flex", justifyContent: "center", margin: "6%" }}>
+        {props.phonetic.audio.length > 0 ? (
+          <div>
+            <br />
+            <div style={{ margin: "2%" }}>{props.phonetic.text}</div>
+            <br />
+            <audio
+              src={props.phonetic.audio}
+              type="audio/mpeg"
+              controls
+            ></audio>
+          </div>
+        ) : null}
       </div>
-  )
+    );
+  } else {
+    return null;
+  }
 }
