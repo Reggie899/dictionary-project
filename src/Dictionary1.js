3af93c0b8d20209e3683309bb27b50ca32b2bc64
Reggie899 <regina.schmidt1212@gmail.com>
2022-06-06 15:27:39 +0200

    update css

--- src/Dictionary1.js
+++ src/Dictionary1.js
@@ -1,79 +1,84 @@
-import React, {useEffect, useState, useContext} from "react";
-import './App.css';
+import React, { useEffect, useState, useContext } from "react";
+import "./App.css";
+import "./Dictionary1.css"
 import axios from "axios";
-import Result from "./Result"
+import Result from "./Result";
 
 export default function Dictionary1(props) {
-const [keyword, setKeyword] = useState("");
-let [result, setResult] = useState(null);
+  const [keyword, setKeyword] = useState("");
+  let [result, setResult] = useState(null);
 
-function handleResponse(response) {
+  function handleResponse(response) {
     console.log(response.data);
     console.log(response.data[0].meanings[0].definitions[0].definition);
-setResult(response.data[0]);
+    setResult(response.data[0]);
+  }
 
-} 
-
-function search(event) {
+  function search(event) {
     event.preventDefault();
-// alert(`Searching for ${keyword} defnition`)
-let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-axios.get(apiUrl).then(handleResponse);
-}
+    // alert(`Searching for ${keyword} defnition`)
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-
-
-
-    return (
+  return (
+    <div>
+      <h1 className="dictionaryh1">
         <div>
- <h1 className="dictionaryh1"><div>Diction<span style={{color: "yellow"}}>a</span><span style={{color: "magenta"}}>r</span></div><div className="tilted">y</div></h1>
-        <div className="SearchSection">
-        {keyword}
-
-        <form
-          id="biggerForm SearchSection"
-          onSubmit={search}
-        //   onChange={(e) => {
-        //     e.preventDefault();
+          Diction<span style={{ color: "yellow" }}>a</span>
+          <span style={{ color: "magenta" }}>r</span>
+        </div>
+        <div className="tilted">y</div>
+      </h1>
+      <div className="SearchSection">
+        {/* {keyword} */}
+        <section>
+          <form
+            id="biggerForm SearchSection"
+            onSubmit={search}
+            //   onChange={(e) => {
+            //     e.preventDefault();
             // setKeyword(e.target.value);
-        //    props.setStart(false);
-// {/* <input type="search" /> */}
+            //    props.setStart(false);
+            // {/* <input type="search" /> */}
             // alert("test");
-        //   }}
-        >
-                      <div className="col-8">
-
-          <input
-            type="search"
-            placeholder="Enter word here 🔍"
-            className="form-control"
-            id="bigForm"
-            // autofocus="on"
-            // autocomplete="on"
-            // onChange={(e) => setTest(e.target.value)}
-            onChange={(e) => setKeyword(e.target.value)}
-
-          />
-                      </div>
-                      <div className="col-4">
-
-            <div className="searchButton">
+            //   }}
+          >
+            <div className="col-8">
               <input
-                type="submit"
-                className="btn btn-light"
-                id="bigButton"
-                value="Search"
+                type="search"
+                placeholder="Enter word here... 👀"
+                className="form-control"
+                id="bigForm"
+                // autofocus="on"
+                // autocomplete="on"
+                // onChange={(e) => setTest(e.target.value)}
+                onChange={(e) => setKeyword(e.target.value)}
               />
             </div>
+            <div className="col-4">
+              {/* <div className="searchButton"> */}
+                <input
+                  type="submit"
+                  // className="btn btn-light"
+                  id="bigButton"
+                  value="🔎"
+                />
+              {/* </div> */}
             </div>
-
-        </form>
-<Result result={result}/>
+          </form>
+        </section>
+        <Result result={result} />
         <div>
-
-        MY GITHUB: <a href="https://github.com/Reggie899/dictionary-project/tree/master/src" target="_blank">Here</a>
+          MY GITHUB:{" "}
+          <a
+            href="https://github.com/Reggie899/dictionary-project/tree/master/src"
+            target="_blank"
+          >
+            Here
+          </a>
         </div>
       </div>
-      </div>
-    )
+    </div>
+  );
 }
--- src/Result.js
+++ src/Result.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
 
 
 export default function Result(props) {
+  const [noResult, setNoResult] = useState(false);
   console.log("Result", props.result);
-
   if (props.result) {
+    // setNoResult(false);
     return <div className="Results">
+<section >
+<h2 style={{display: "flex", justifyContent: "center"}}> {props.result.word} </h2>
 
-<h2>{props.result.word}</h2>
-
+</section>
+{/* <br/> */}
+{props.result.meanings.map(function(meaning, index){
+  return <section className="borderMagenta" key={index}>
+    <Meaning meaning={meaning} />
+    </section>  // meaning.definitions[0].definition;
+})}
+<section>
 {props.result.phonetics.map(function(phonetic, index) {
   return (
     <div key={index}>
@@ -19,14 +28,16 @@ export default function Result(props) {
       </div>
   )
 })}
-
-{props.result.meanings.map(function(meaning, index){
-  return <div key={index}>
-    <Meaning meaning={meaning} />
-    </div>  // meaning.definitions[0].definition;
-})}
+</section>
     </div>;
   } else {
-      return null;
+//     setNoResult(true);
+
+      return (
+        <div>
+{noResult ? <h2>Sorry, the dictionary doesn't cover this word.</h2> : null}
+</div>
+      )
+// return null;
   }
 }
